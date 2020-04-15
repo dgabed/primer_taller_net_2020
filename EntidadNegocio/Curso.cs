e44d0147343decfe6cd6c2fa79a11419c96672cd
dgabed <dtgabriel@gmail.com>
2020-04-15 15:56:30 -0300

    Primer commit rehecho clases alumno y curso iniciales

---
+++ EntidadNegocio/Curso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadNegocio
+{
+    class Curso
+    {
+        public string descripcion { get; set; }
+        public Alumno alumno;
+
+    }
+}
