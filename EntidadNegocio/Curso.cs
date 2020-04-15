92ad4b03073d64fe28dd847776333c0c51f1fa17
dgabed <dtgabriel@gmail.com>
2020-04-15 16:20:15 -0300

    nom_propiedad_modificado

--- EntidadNegocio/Curso.cs
+++ EntidadNegocio/Curso.cs
@@ -8,7 +8,7 @@ namespace EntidadNegocio
 {
     class Curso
     {
-        public string master_descripcion { get; set; }
+        public string master_descripcion_conflicto { get; set; }
         private List<Alumno> alumnos = new List<Alumno>();
 
         public void agregarAlumno(Alumno a)
